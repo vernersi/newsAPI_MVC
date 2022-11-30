2628f51730f7bf9e5097e7ca0bb756f24fdc876b
vernersi <92396778+vernersi@users.noreply.github.com>
2022-11-30 09:17:37 +0200

    Delete Twig/views directory

--- Twig/views/article.php
+++
@@ -1,8 +0,0 @@
-<h3>{{articleTitle}}</h3>
-<p class="lead">
-    {{articleContent}}
-</p>
-<a href="{{articleUrl}}">
-    <img src="{{articleImgUrl}}" alt="img URL" style="width:20%;height:auto">
-</a>
-
